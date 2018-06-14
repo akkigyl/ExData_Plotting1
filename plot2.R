9002793450f3eb3f69d00997b565e02e15036699
akkigyl <akkigyl@gmail.com>
2018-06-14 18:31:51 +0530

    ExploratoryDataAnalysis-Assignment

--- plot2.R
+++ plot2.R
@@ -1,25 +1,16 @@
-
 cls <- c(Voltage="numeric", Global_active_power="numeric",
          Global_intensity="numeric",Sub_metering_1="numeric",
          Sub_metering_2="numeric",Sub_metering_3="numeric",
          Global_active_power="numeric",Global_reactive_power="numeric")
-
-#read data
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",
                    dec = ".", stringsAsFactors = FALSE, na.strings = "?",
                    colClasses = cls)
 
 energyData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-as.Date(energyData$Date)
-
-#deleted all the rows that had NA values
 energyData <-na.omit(energyData)
+datetime <- strptime(paste(energyData$Date, energyData$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+png("plot2.png", width=480, height=480)
 
-#plot data
-png("plot1.png", width=480, height=480)
-hist(energyData$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", 
-     ylab = "Frequency", main="Global Active Power")
-dev.off()       
-       
-       
+plot(datetime, energyData$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
--- plot3.R
+++ plot3.R
@@ -1,25 +1,18 @@
-
 cls <- c(Voltage="numeric", Global_active_power="numeric",
          Global_intensity="numeric",Sub_metering_1="numeric",
          Sub_metering_2="numeric",Sub_metering_3="numeric",
          Global_active_power="numeric",Global_reactive_power="numeric")
-
-#read data
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",
                    dec = ".", stringsAsFactors = FALSE, na.strings = "?",
                    colClasses = cls)
 
 energyData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-as.Date(energyData$Date)
-
-#deleted all the rows that had NA values
 energyData <-na.omit(energyData)
-
-#plot data
-png("plot1.png", width=480, height=480)
-hist(energyData$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", 
-     ylab = "Frequency", main="Global Active Power")
-dev.off()       
-       
-       
+datetime <- strptime(paste(energyData$Date, energyData$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+png("plot3.png", width=480, height=480)
+plot(datetime, energyData$Sub_metering_1, type = "l", ylab = "Energy Submetering", xlab = "")
+lines(datetime, energyData$Sub_metering_2, type="l", col="red")
+lines(datetime, energyData$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
